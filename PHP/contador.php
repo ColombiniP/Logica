ae2f5a4483f439f355e0536e7d6d4edaa0314e69
Paschoal Colombini <paschoal.colombini@live.com>
2024-08-09 13:40:32 -0300

    organizando pastas

---
+++ PHP/contador.php
@@ -0,0 +1,8 @@
+<?php
+
+# Escreva um programa que imprima os números de 1 a 100.
+# Para multiplos de 3, imprima "Fizz" em vez do número 
+# Para multiplos de 5, imprima "Buzz" 
+# Para número que são multiplos de ambos, imprima "FizzBuzz"
+
+echo "Olá mundo";
